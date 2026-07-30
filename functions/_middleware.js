2fc7a47cdd520db3c0803b576ba9577545ee07a2
Solomon Cho <solomoncho@MacBook-Pro-2.local>
2026-07-30 16:26:23 -0400

    fix: remove middleware for CI test

--- functions/_middleware.js
+++
@@ -1,7 +0,0 @@
-import { enforceCloudflareApiRateLimit } from "./_lib/apiRateLimit.js";
-
-export async function onRequest(context) {
-  const blocked = await enforceCloudflareApiRateLimit(context);
-  if (blocked) return blocked;
-  return context.next();
-}
